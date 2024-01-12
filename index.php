f4f3d60d3cafc08809a97797d087530c181f55a2
Marcelo <joao69@aluno.senai-ce.org.br>
2024-01-12 20:57:23 -0300

    atualizando

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php
+    include 'header.php';
+    ?>
+
+<body>
+    <div class="wrapper">
+
+        <?php
+        include 'menu.php';
+        ?>
+        <div class="main">
+        <?php
+        include 'top.php'; 
+        ?>
+        <?php
+        include 'body.php' 
+        ?>
+
+
+
+            <footer class="footer">
+            <?php
+            include 'footer.php'; 
+            ?>
+            </footer>
+        </div>
+    </div>
+
+    <script src="js/app.js"></script>
+
+</body>
+
+</head>
+
+</html>
